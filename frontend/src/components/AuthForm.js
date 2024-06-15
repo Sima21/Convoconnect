2c538240d12c04eff63af725e82ea29f4a6e2175
mj-gsl <gsl.produktiion@gmail.com>
2024-06-15 21:12:22 +0200

    AuthForm page is edited , Background effect, sidebar effect, new container added and etc.

--- frontend/src/components/AuthForm.js
+++ frontend/src/components/AuthForm.js
@@ -1,14 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { createUser, loginUser } from '../api';
+import React, { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import './CommonStyles.css';
+import './AuthForm.css';
+import LogoImage from './Logo2.png'; // Ensure the path to the image is correct
 
 function AuthForm() {
-    const [isLogin, setIsLogin] = useState(true);
-    const [userData, setUserData] = useState({ username: '', email: '', password: '' });
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState('');
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -25,80 +22,87 @@ function AuthForm() {
         };
     }, []);
 
-    const handleSubmit = async (event) => {
-        event.preventDefault();
-        setLoading(true);
-        setError('');
-        try {
-            const response = isLogin ? await loginUser(userData) : await createUser(userData);
-            if (response.data && response.data.token) {
-                localStorage.setItem('token', response.data.token);
-                localStorage.setItem('username', response.data.username || userData.username);
-                navigate('/dashboard');
-            } else {
-                throw new Error('Authentication failed, no token provided');
-            }
-        } catch (err) {
-            setError(err.response ? err.response.data.message : err.message);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    const handleChange = (e) => {
-        setUserData({ ...userData, [e.target.name]: e.target.value });
-    };
-
     const handleGoogleLogin = () => {
         window.location.href = process.env.REACT_APP_GOOGLE_AUTH_URL || 'http://localhost:5000/api/auth/google';
     };
 
     return (
         <div className="container">
-            <form className="form-container" onSubmit={handleSubmit}>
-                <label>
-                    Username:
-                    <input className="input-field" type="text" name="username" value={userData.username} onChange={handleChange} required />
-                </label>
-                {!isLogin && (
-                    <label>
-                        Email:
-                        <input className="input-field" type="email" name="email" value={userData.email} onChange={handleChange} required />
-                    </label>
-                )}
-                <label>
-                    Password:
-                    <input className="input-field" type="password" name="password" value={userData.password} onChange={handleChange} required />
-                </label>
-                {error && <p style={{ color: 'red' }}>{error}</p>}
-                <motion.button
-                    className="button auth-button-login"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                    type="submit"
-                    disabled={loading}
-                >
-                    {isLogin ? 'Login' : 'Register'}
-                </motion.button>
-                <motion.button
-                    className="button auth-button-toggle"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                    type="button"
-                    onClick={() => setIsLogin(!isLogin)}
-                >
-                    {isLogin ? 'Need to register?' : 'Already have an account?'}
-                </motion.button>
-                <motion.button
-                    className="button auth-button-google"
-                    whileHover={{ scale: 1.05 }}
-                    whileTap={{ scale: 0.95 }}
-                    type="button"
-                    onClick={handleGoogleLogin}
-                >
-                    Login with Google
-                </motion.button>
-            </form>
+            <div className="sidebar">
+                <div className="logo-container">
+                    <div className="circle-image"></div>
+                </div>
+                <div className="sidebar-item">
+                    <span className="text">Developers Team</span>
+                    <span className="icon">▶</span>
+                    <div className="sidebar-info">
+                        Sima,<br/> 
+                        Lotfi,<br/>
+                        Mojtaba<br/>
+                    </div>
+                </div>
+                <div className="sidebar-item">
+                    <span className="text">CONTACT</span>
+                    <span className="icon">▶</span>
+                    <div className="sidebar-info">
+                        mojtaba.pourshiri@docc.techsta.com
+                    </div>
+                </div>
+                <div className="sidebar-item">
+                    <span className="text">ABOUT</span>
+                    <span className="icon">▶</span>
+                    <div className="sidebar-info">
+                        This is the final project
+                    </div>
+                </div>
+            </div>
+            <div className="main-content">
+                <div className="button-container">
+                    <motion.button
+                        className="button auth-button-toggle"
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        type="button"
+                        onClick={() => navigate('/register')}
+                    >
+                        Need to register?
+                    </motion.button>
+                    <motion.button
+                        className="button auth-button-google"
+                        whileHover={{ scale: 1.05 }}
+                        whileTap={{ scale: 0.95 }}
+                        type="button"
+                        onClick={handleGoogleLogin}
+                    >
+                        Login with Google
+                    </motion.button>
+                </div>
+                <div className="form-container">
+                    <div className="header-with-logo">
+                        <img src={LogoImage} alt="ConvoConnect Logo" className="header-logo" />
+                        <motion.div className="auth-header" initial={{ y: 100, opacity: 0.2 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1 }}>
+                            ConvoConnect
+                        </motion.div>
+                    </div>
+                    <motion.div className="auth-subheader" initial={{ y: 100, opacity: 0.2 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1.2 }}>
+                        Overview
+                    </motion.div>
+                    <motion.div className="auth-text" initial={{ y: 100, opacity: 0.2 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1.4 }}>
+                        ConvoConnect is a video conferencing platform developed as a DevOps project that incorporates technologies and methodologies. It utilizes Jitsi Meet for real-time communication and integrates Authentik for user authentication, showcasing a modern, scalable application built on Amazon EKS.
+                    </motion.div>
+                    <motion.div className="auth-subheader" initial={{ y: 100, opacity: 0.2 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1.6 }}>
+                        Features
+                    </motion.div>
+                    <motion.div className="auth-text" initial={{ y: 100, opacity: 0.2 }} animate={{ y: 0, opacity: 1 }} transition={{ duration: 1.8 }}>
+                        <ul style={{ textAlign: 'left', color: 'black' }}>
+                            <li>Authentik Integration: Secure user authentication ensuring flexibility and security.</li>
+                            <li>Jitsi Meet: High-quality video conferencing embedded within the application.</li>
+                            <li>Group Management: Enables creation and management of user groups for organized communications.</li>
+                            <li>Dashboard: Personalized user dashboard displaying scheduled meetings and active groups.</li>
+                        </ul>
+                    </motion.div>
+                </div>
+            </div>
         </div>
     );
 }
